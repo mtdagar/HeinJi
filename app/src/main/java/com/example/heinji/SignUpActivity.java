b81765cd63e896f6dd630bc8cb55f567f52d42ee
mtdagar <mtdagar@gmail.com>
2021-04-05 16:05:31 +0530

    connection with firebase

--- app/src/main/java/com/example/heinji/SignUpActivity.java
+++ app/src/main/java/com/example/heinji/SignUpActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.heinji.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
+        auth = FirebaseAuth.getInstance();
+
+
     }
 }
