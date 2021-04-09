21c180085234ab9940a797e9da901582f319f897
mtdagar <mtdagar@gmail.com>
2021-04-09 16:56:32 +0530

    user display sample

---
+++ app/src/main/java/com/example/heinji/Adapters/UserAdapter.java
@@ -0,0 +1,66 @@
+package com.example.heinji.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.heinji.Models.Users;
+import com.example.heinji.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+
+    ArrayList<Users> list;
+    Context context;
+
+    public UserAdapter(ArrayList<Users> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_show_user, parent , false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Users users = list.get(position);
+        Picasso.get().load(users.getProfilepic()).placeholder(R.drawable.avatar).into(holder.image);
+        holder.userName.setText(users.getUserName());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView userName, lastMessage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.profile_image);
+            userName = itemView.findViewById(R.id.userNameList);
+            lastMessage = itemView.findViewById(R.id.lastMessage);
+        }
+    }
+
+}
